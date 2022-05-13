2f0f0d32021ec486555547f796c0977a9d22a865
Denis <denison4@mail.ru>
2022-05-13 18:17:11 +0300

    bugfix

--- pages/api/loadPosts.tsx
+++
@@ -1,7 +0,0 @@
-
-export const loadPosts = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=21&_page=0`)
-    const posts = await response.json()
-
-    return posts
-}
